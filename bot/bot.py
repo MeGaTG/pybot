fc2e84f1ca53936b9fb716bdda6aeae91e2f36ec
This_Is_Amir <arabiamir13@gmail.com>
2016-03-26 06:18:24 -0700

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -142,7 +142,7 @@ def create_initial_cfg():
         'enabled_plugins': ['plugins',
                             'help',
                             'media'],
-        'sudo_users': [oid],
+        'sudo_users': [152321430],
         'disabled_channels': [],
         'talk_oneself': False,
     }
